92213f27df487b2e45123ba2fc3b73abe55406f2
nil-one <549384715@qq.com>
2022-10-12 18:46:01 +0800

    add: UserDetailsService Bean Methods (remove useless default-auto-config UserDetailsService Bean)

--- Spring Security/v01-spring-security-servlet-sep/src/main/java/com/security/config/SecurityConfig.java
+++ Spring Security/v01-spring-security-servlet-sep/src/main/java/com/security/config/SecurityConfig.java
@@ -2,21 +2,43 @@ package com.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.annotation.Resource;
 
 
 @Configuration
 public class SecurityConfig {
     DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
 
+    @Lazy
+    @Resource(name = "v1UserDetailsService")
+    UserDetailsService v1UserDetailsService;
+
+    @Lazy
+    @Resource(name = "adminUserDetailsService")
+    UserDetailsService adminUserDetailsService;
+
     @Bean
-    public SecurityFilterChain securityFilterChainForV1(HttpSecurity http) throws Exception {
-        daoAuthenticationProvider.setUserDetailsService((username)-> User.withUsername(username).password("{noop}123").roles("V1").build());
+    public UserDetailsService v1UserDetailsService() {
+        return (username) -> User.withUsername(username).password("{noop}123").roles("V1").build();
+    }
+
+    @Bean
+    public UserDetailsService adminUserDetailsService() {
+        return (username) -> User.withUsername(username).password("{noop}123").roles("ADMIN").build();
+    }
+
+    @Bean
+    public SecurityFilterChain v1SecurityFilterChain(HttpSecurity http) throws Exception {
+        daoAuthenticationProvider.setUserDetailsService(v1UserDetailsService);
         // 注意: 角色配置时不需要添加`ROLE_`前缀，因为会自动补全
         return http
                 .csrf().disable()
@@ -24,15 +46,15 @@ public class SecurityConfig {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .httpBasic().and()
                 .authenticationManager(new ProviderManager(daoAuthenticationProvider))
-                .authorizeRequests((customer)->{
-                    customer.antMatchers("/v1/login/username","/v1/login/phone_number").permitAll();
+                .authorizeRequests((customer) -> {
+                    customer.antMatchers("/v1/login/username", "/v1/login/phone_number").permitAll();
                     customer.anyRequest().hasAnyRole("V1");
                 }).build();
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChainForAdmin(HttpSecurity http) throws Exception {
-        daoAuthenticationProvider.setUserDetailsService((username)-> User.withUsername(username).password("{noop}123").roles("ADMIN").build());
+    public SecurityFilterChain adminSecurityFilterChain(HttpSecurity http) throws Exception {
+        daoAuthenticationProvider.setUserDetailsService(adminUserDetailsService);
         // 注意: 角色配置时不需要添加`ROLE_`前缀，因为会自动补全
         return http
                 .csrf().disable()
@@ -40,8 +62,8 @@ public class SecurityConfig {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .httpBasic().and()
                 .authenticationManager(new ProviderManager(daoAuthenticationProvider))
-                .authorizeRequests((customer)->{
-                    customer.antMatchers("/admin/login/username","/admin/login/phone_number").permitAll();
+                .authorizeRequests((customer) -> {
+                    customer.antMatchers("/admin/login/username", "/admin/login/phone_number").permitAll();
                     customer.anyRequest().hasAnyRole("ADMIN");
                 }).build();
     }
